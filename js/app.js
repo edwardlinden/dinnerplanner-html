0b253f5edc51dfe4e796752c4fbd289fbbaa903e
Henrik Waardahl <Hwaa@kth.se>
2019-01-22 22:58:48 +0100

    komp 3

--- js/app.js
+++ js/app.js
@@ -3,13 +3,9 @@
 	var model = new DinnerModel();
 	
 	// And create the instance of ExampleView
-<<<<<<< HEAD
 	var sideBarView = new sidebarView($("#sidebarView"), model);
 	var dishSearchView = new dishSearchView($("#dishSearchView"), model);
 
-=======
-	var picture_baby = new ExampleView($("#exampleView"));
->>>>>>> d7dde27bf4ad4cf7ad705ae86322c70e5beb127d
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
